586b31c662cd171ccf5df69c0c57c8e535515187
Emily Shepherd <emily.shepherd@wearetwogether.com>
2017-03-05 01:41:19 +0000

    Add description of DoctrineFix

--- src/AppBundle/Traits/DoctrineFix.php
+++ src/AppBundle/Traits/DoctrineFix.php
@@ -2,6 +2,15 @@
 
 namespace JustMeet\AppBundle\Traits;
 
+/**
+ * This trait is required because symfony twig refuses to attempt to
+ * access a property if it's not natively public, even if a __get
+ * method is present. It will, however, call it as a method if a __call
+ * method exists, so this trait hacks that into Enties.
+ *
+ * Will consider moving into spaark/composite-utils in a slightly more
+ * neat way
+ */
 trait DoctrineFix
 {
     public function __call($method, $args)
