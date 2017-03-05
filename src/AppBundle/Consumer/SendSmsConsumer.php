a4ce9ed902ab12b8d0db4aa61ce454dfee937021
Emily Shepherd <emily.shepherd@wearetwogether.com>
2017-03-05 04:32:37 +0000

    Code style fixes

--- src/AppBundle/Consumer/SendSmsConsumer.php
+++ src/AppBundle/Consumer/SendSmsConsumer.php
@@ -3,18 +3,23 @@ namespace JustMeet\AppBundle\Consumer;
 
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
-use Nexmo\Client;
+use Nexmo\Client as NexmoClient;
+use Nexmo\Client\Credentials\Basic as BasicCredentials;
 
 
 class SendSmsConsumer implements ConsumerInterface 
 {
     public function execute(AMQPMessage $msg)
     {
-        //Process picture upload.
-        //$msg will be an instance of `PhpAmqpLib\Message\AMQPMessage` with the $msg->body being the data sent over RabbitMQ.
+        // Process picture upload.
+        // $msg will be an instance of `PhpAmqpLib\Message\AMQPMessage`
+        // with the $msg->body being the data sent over RabbitMQ.
         fwrite(STDOUT, "Excecuting Queue Item! ".$msg->body."\n" );
 
-        $client = new \Nexmo\Client(new \Nexmo\Client\Credentials\Basic('35c8329c','a9f5b169560927ac'));
+        $client = new NexmoClient(new BasicCredentials
+        (
+            '35c8329c', 'a9f5b169560927ac'
+        ));
 
         $message = $client->message()->send([
             'to' => "+447834228887",
@@ -24,11 +29,6 @@ class SendSmsConsumer implements ConsumerInterface
         
         $isTaskSuccess = true;
         if (!$message) {
-            // If your image upload failed due to a temporary error you can return false
-            // from your callback so the message will be rejected by the consumer and
-            // requeued by RabbitMQ.
-            // Any other value not equal to false will acknowledge the message and remove it
-            // from the queue
             return false;
         }
     }
